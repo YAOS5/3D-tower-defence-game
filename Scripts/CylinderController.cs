6cc39e22f627e22e3ef85f6e4df1d794b5515626
Peter Shi <yaos5@student.unimelb.edu.au>
2019-09-22 20:33:44 +1000

    making some progress re creating the tower

---
+++ Scripts/CylinderController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderController : MonoBehaviour{
+    public float xcoor;
+    public float ycoor;
+    public float zcoor;
+
+    public GameObject projectilePrefab;
+
+
+    void Start()
+    {
+        // for testing purpose just going to set
+        // xyz to the origin
+        // delete this later
+        xcoor = ycoor = zcoor = 0;
+        this.gameObject.transform.position = new Vector3(xcoor, ycoor, zcoor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // code from workshop
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate<GameObject>(projectilePrefab);
+            projectile.transform.position = this.gameObject.transform.position;
+        }
+    }
+
+
+    //public static void initializeTowerPosition(float x, float y, float z)
+    //{
+    //}
+}
