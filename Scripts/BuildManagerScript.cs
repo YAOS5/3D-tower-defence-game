4273415efe1b197f594a98da354d5171f28981ef
Lachie James <47808687+lachiejames@users.noreply.github.com>
2019-09-24 23:34:16 +1000

    Lachies commit

---
+++ Scripts/BuildManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManagerScript : MonoBehaviour {
+
+    // The C# version of the singleton pattern
+    public static BuildManagerScript instance;
+    void Awake() {
+        if (instance != null) {
+            Debug.LogError("Why do we have more than 1 build manager???");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+    public GameObject missileLauncherPrefab;
+
+    private TurretBlueprint turretBlueprint;
+
+    // A 'property' basically an anonymous function?
+    public bool canBuild { get { return turretBlueprint != null; } }
+    public bool hasMoney { get { return PlayerStatsScript.money >= turretBlueprint.cost; } }
+
+    public void BuildTurretOn(NodeScript nodeScript) {
+
+        if (PlayerStatsScript.money < turretBlueprint.cost) {
+            Debug.Log("You're broke bitch!");
+            return;
+        }
+
+        PlayerStatsScript.money -= turretBlueprint.cost;
+
+        GameObject turret = Instantiate(turretBlueprint.prefab, nodeScript.GetBuildPosition(), Quaternion.identity);
+        nodeScript.turret = turret;
+
+        Debug.Log("turret build!  you have $" + PlayerStatsScript.money);
+    }
+
+    public void SelectTurretToBuild(TurretBlueprint turretBlueprint) {
+        this.turretBlueprint = turretBlueprint;
+    }
+
+}
---
+++ Scripts/CameraControllerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerScript : MonoBehaviour {
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public bool movementEnabled = true;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // Pressing escape will enable/disable movement
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            movementEnabled = !movementEnabled;
+        }
+
+        if (!movementEnabled) {
+            return;
+        }
+
+        // Space.world makes it move forward without regard to camera rotation, this will be useful
+        if (Input.GetKey("w")) {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s")) {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d")) {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a")) {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        // Allows us to zoom in using the mouse wheel
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 currentPosition = transform.position;
+        currentPosition.y -= scroll * scrollSpeed * Time.deltaTime;
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+        transform.position = currentPosition;
+
+    }
+}
